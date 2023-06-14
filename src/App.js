06b6f93cba6a1cc2635783fdf3926ee86303aef7
Rajendrankr25 <rajendrankr25@gmail.com>
2023-06-14 10:02:54 +0530

    api2 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Api1 from './components/api1';
+import Api1 from './components/Api1';
+import Api2 from './components/Api2';
 
 function App() {
   return (
     <div className="App">
       <Api1 />
+      <Api2 />
     </div>
   );
 }
---
+++ src/components/Api2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Api2() {
+    return (
+        <div>Api2</div>
+    )
+}
+
+export default Api2
