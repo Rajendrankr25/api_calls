39fd504263e13f3276c56babf6b6e2285f806e5e
Rajendrankr25 <rajendrankr25@gmail.com>
2023-06-15 13:09:59 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Api1 from './components/Api1';
 import Api2 from './components/Api2';
@@ -7,12 +8,32 @@ import Api4 from './components/Api4';
 function App() {
   return (
     <div className="App">
-      {/* <Api1 /> */}
-      {/* <Api2 /> */}
-      {/* <Api3 /> */}
-      <Api4 />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/api1' element={<Api1 />} />
+        <Route path='/api2' element={<Api2 />} />
+        <Route path='/api3' element={<Api3 />} />
+        <Route path='/api4' element={<Api4 />} />
+      </Routes>
     </div>
   );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>API Learning Task</h1>
+      <p>Please click below links for appropriate API's</p>
+      <br />
+      <Link to={'/api1'} target='_blank'>API 1 - Movie Data API</Link>
+      <br />
+      <Link to={'/api2'} target='_blank'>API 2 - Fake Store API</Link>
+      <br />
+      <Link to={'/api3'} target='_blank'>API 3 - Users Data API</Link>
+      <br />
+      <Link to={'/api4'} target='_blank'>API 4 - Drinks Data API</Link>
+    </div>
+  )
 }
 
 export default App;
